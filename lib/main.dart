d6cf764cb0a34b4e2028628463f0da1eb8f569c5
Sai Sidharth <shrichit69@gmail.com>
2023-08-16 20:12:29 +0530

    Catelog Class created

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:first_flutter_app/pages/login_page.dart';
 import 'package:first_flutter_app/utils/routes.dart';
 import 'package:first_flutter_app/widgets/themes.dart';
 import "package:flutter/material.dart";
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   //Main Entry Point
